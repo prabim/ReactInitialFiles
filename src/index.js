5471b72c7b9989ae32a58c78b7a799db33d6573d
Prabim <prabeam@gmail.com>
2018-06-24 09:36:38 +0545

    Commit files related to redux.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+
+const  createStoreWirhMiddleware = applyMiddleware()(createStore );
+
+ReactDOM.render(
+    <Provider store={createStoreWirhMiddleware}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
